22594588942d8a1bdef57ec33d46a1fb76b4642d
Protsko Polina <protskopolina1@gmail.com>
2025-05-26 23:02:00 +0300

    Add files via upload

---
+++ LB9.1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace LB9._1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введіть шлях до файлу:");
+            string path = Console.ReadLine();
+            if (File.Exists(path))
+            {
+                string text = File.ReadAllText(path);
+                //Підрахунок кількості речень за розділовими знаками 
+                int sentenseCount = Regex.Matches(text, @"[.!?]+").Count;
+                //Кількість велких літер 
+                int upperCount = text.Count(char.IsUpper);
+                //Кількість малих літер
+                int lowerCount = text.Count(char.IsLower);
+                //Голосні літери 
+                string vowelsList = "аеєиіїоуюяaeiouAEIOUАЕЄИІЇОУЮЯ";
+                int vowelCount = text.Count(c => vowelsList.Contains(c));
+                // Приголосні
+                int consonantCount = text.Count(c => char.IsLetter(c) && !vowelsList.Contains(c));
+                //Кількість цифр
+                int digitCount = text.Count(char.IsDigit);
+
+                Console.WriteLine($"Кількості речень: {sentenseCount}");
+                Console.WriteLine($"Кількість великих літер: {upperCount}");
+                Console.WriteLine($"Кількість малих літер: {lowerCount}");
+                Console.WriteLine($"Кількість голосних літер: {vowelCount}");
+                Console.WriteLine($"Кількість приголосних літер: {consonantCount}");
+                Console.WriteLine($"Кількість цифр: {digitCount}");
+            }
+            else
+            {
+                Console.WriteLine("File doesnt exists");
+            }
+
+        }
+    }
+}
---
+++ LB9.2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace LB9._2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("ВВедіть шлях до файлу : ");
+            string textPath = Console.ReadLine();
+
+            Console.Write("Введіть шлях до файлу зі словами для цензури: ");
+            string wordsPath = Console.ReadLine();
+
+            if (File.Exists(textPath)&& File.Exists(wordsPath))
+            {
+                string text = File.ReadAllText(textPath);
+                string[] bannedWords = File.ReadAllLines(wordsPath);
+
+                foreach (string word in bannedWords)
+                {
+                    string pattern = $@"\b{Regex.Escape(word)}\b";
+                    string replacement = new string('*', word.Length);
+                    text = Regex.Replace(text, pattern, replacement, RegexOptions.IgnoreCase);
+
+                    string outputPath = "result.txt";
+                    File.WriteAllText(outputPath, text);
+                    Console.WriteLine($"результат записано у файл: {outputPath}");
+
+                } 
+
+            } else
+            {
+                Console.WriteLine("Один аб два файла не знайдено.");
+            }
+
+                
+        }
+    }
+}
